a49eeb2df78b66b9d5ae4fb09d630ca2cc5453e2
Santi Aguilera <santiago.aguilera@mercadolibre.com>
2022-05-16 22:59:18 -0300

    Minor performance boosts in concurrency generics

--- concurrent.go
+++ concurrent.go
@@ -3,6 +3,8 @@ package pipeline
 import (
 	"context"
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 type (
@@ -12,6 +14,12 @@ type (
 	}
 
 	reducer[O any] func(context.Context, O, O) (O, error)
+
+	// concurrentSlice that allows appending values of type T concurrently
+	concurrentSlice[T any] struct {
+		sync.RWMutex
+		v []T
+	}
 )
 
 // NewConcurrentStep creates a step that will run each of the inner steps concurrently.
@@ -41,20 +49,78 @@ func (c ConcurrentStep[I, O]) Run(ctx context.Context, in I) (O, error) {
 		return *new(O), errors.New("cannot run with empty concurrent steps")
 	}
 
-	mres, err := runConcurrently(c.steps, func(s Step[I, O]) (O, error) {
-		return s.Run(ctx, in)
-	})
-
+	mres, err := c.runConcurrently(ctx, c.steps, in)
 	if err != nil {
 		return *new(O), err
 	}
 
 	acc := mres[0]
-	for _, v := range mres[1:] {
-		acc, err = c.reduce(ctx, acc, v)
+	for i := 1; i < len(mres); i++ {
+		acc, err = c.reduce(ctx, acc, mres[i])
 		if err != nil {
 			return *new(O), err
 		}
 	}
 	return acc, nil
+}
+
+// Run a number of workers concurrently, waiting for all of them to finish.
+// After they're all done, if one of them failed the error is returned.
+// If more than one fails, the last error is returned
+func (c ConcurrentStep[I, O]) runConcurrently(
+	ctx context.Context,
+	workers []Step[I, O],
+	in I,
+) ([]O, error) {
+
+	var wg sync.WaitGroup
+	var errResult atomic.Value
+	var mergedRes concurrentSlice[O]
+
+	wg.Add(len(workers))
+	if len(workers) > 1 {
+		for i := 0; i < len(workers); i++ {
+			go c.runStep(&wg, &errResult, &mergedRes, ctx, in, workers[i])
+		}
+		wg.Wait()
+	} else { // avoid concurrency, no need to spawn and wait just use current
+		c.runStep(&wg, &errResult, &mergedRes, ctx, in, workers[0])
+	}
+
+	if err, ok := errResult.Load().(error); ok && err != nil {
+		return nil, err
+	}
+	return mergedRes.Get(), nil
+}
+
+func (c ConcurrentStep[I, O]) runStep(
+	wg *sync.WaitGroup,
+	errResult *atomic.Value,
+	mergedRes *concurrentSlice[O],
+	ctx context.Context,
+	in I,
+	step Step[I, O],
+) {
+
+	res, err := step.Run(ctx, in)
+	if err != nil {
+		errResult.CompareAndSwap(nil, err)
+	}
+
+	mergedRes.Append(res)
+	wg.Done()
+}
+
+// Read slice T stored inside
+func (s *concurrentSlice[T]) Get() []T {
+	s.RLock()
+	defer s.RUnlock()
+	return s.v
+}
+
+// Append safely inside T slice
+func (s *concurrentSlice[T]) Append(t T) {
+	s.Lock()
+	defer s.Unlock()
+	s.v = append(s.v, t)
 }
--- pipeline_benchmark_test.go
+++ pipeline_benchmark_test.go
@@ -3,9 +3,7 @@ package pipeline_test
 import (
 	"context"
 	"flag"
-	"fmt"
 	"os"
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,97 +13,49 @@ import (
 
 var render = flag.Bool("pipeline.render", false, "render pipeline")
 
-func NewStringToIntStep(number **int) pipeline.Step[string, int] {
-	current := **number
-	next := current + 1
-	*number = &next
-
-	return pipeline.NewUnitStep(fmt.Sprintf("Step %d", current), func(ctx context.Context, in string) (int, error) {
-		vi, err := strconv.Atoi(in)
-		if err != nil {
-			return 0, err
-		}
-		return vi + 1, nil
+func newDummyStep() pipeline.Step[int, int] {
+	return pipeline.NewUnitStep("dummy step", func(ctx context.Context, in int) (int, error) {
+		return in, nil
 	})
 }
 
-func NewStringToStringStep(number **int) pipeline.Step[string, string] {
-	current := **number
-	next := current + 1
-	*number = &next
-
-	return pipeline.NewUnitStep(fmt.Sprintf("Step %d", current), func(ctx context.Context, in string) (string, error) {
-		vi, err := strconv.Atoi(in)
-		if err != nil {
-			return "", err
-		}
-		return strconv.FormatInt(int64(vi+1), 10), nil
-	})
+func dummyReducer(_ context.Context, a, _ int) (int, error) {
+	return a, nil
 }
 
-func NewIntToStringStep(number **int) pipeline.Step[int, string] {
-	current := **number
-	next := current + 1
-	*number = &next
+func NewImmenseGraph() pipeline.Step[int, int] {
+	step := newDummyStep()
 
-	return pipeline.NewUnitStep(fmt.Sprintf("Step %d", current), func(ctx context.Context, in int) (string, error) {
-		return strconv.FormatInt(int64(in+1), 10), nil
-	})
-}
-
-func NewIntToIntStep(number **int) pipeline.Step[int, int] {
-	current := **number
-	next := current + 1
-	*number = &next
-
-	return pipeline.NewUnitStep(fmt.Sprintf("Step %d", current), func(ctx context.Context, in int) (int, error) {
-		return in + 1, nil
-	})
-}
-
-func intReducer(_ context.Context, a, b int) (int, error) {
-	return a + b, nil
-}
-
-func NewImmenseGraph() pipeline.Step[string, int] {
-	count := 0
-	refcount := &count
-
-	intToInt := NewIntToIntStep(&refcount)
-	stringToInt := NewStringToIntStep(&refcount)
-	stringToString := NewStringToStringStep(&refcount)
-	intToString := NewIntToStringStep(&refcount)
-
-	innerJob := pipeline.NewSequentialStep[string, string, int](
-		pipeline.NewSequentialStep[string, int, string](
-			pipeline.NewSequentialStep(stringToInt, intToInt),
-			pipeline.NewSequentialStep(intToString, stringToString),
+	innerJob := pipeline.NewSequentialStep[int, int, int](
+		pipeline.NewSequentialStep[int, int, int](
+			pipeline.NewSequentialStep(step, step),
+			pipeline.NewSequentialStep(step, step),
 		),
 		pipeline.NewConcurrentStep(
-			[]pipeline.Step[string, int]{stringToInt, stringToInt, stringToInt, stringToInt, stringToInt, stringToInt}, intReducer,
+			[]pipeline.Step[int, int]{step, step, step, step, step, step}, dummyReducer,
 		),
 	)
 
-	return pipeline.NewSequentialStep[string, int, int](
-		pipeline.NewConditionalStep[string, int](
+	return pipeline.NewSequentialStep[int, int, int](
+		pipeline.NewConditionalStep[int, int](
 			pipeline.NewAnonymousStatement(
-				func(ctx context.Context, t string) bool {
+				func(ctx context.Context, t int) bool {
 					return true
 				},
 			),
 			pipeline.NewConcurrentStep(
-				[]pipeline.Step[string, int]{innerJob, stringToInt, innerJob, stringToInt, innerJob, stringToInt, innerJob, stringToInt},
-				intReducer,
+				[]pipeline.Step[int, int]{innerJob, step, innerJob, step, innerJob, step, innerJob, step},
+				dummyReducer,
 			),
 			nil,
 		),
-		pipeline.NewSequentialStep[int, string, int](
-			pipeline.NewSequentialStep[int, int, string](
-				pipeline.NewSequentialStep(intToInt, intToInt),
-				pipeline.NewSequentialStep(intToString, stringToString),
+		pipeline.NewSequentialStep[int, int, int](
+			pipeline.NewSequentialStep[int, int, int](
+				pipeline.NewSequentialStep(step, step),
+				pipeline.NewSequentialStep(step, step),
 			),
 			pipeline.NewConcurrentStep(
-				[]pipeline.Step[string, int]{stringToInt, stringToInt, stringToInt, stringToInt, stringToInt, stringToInt}, intReducer,
+				[]pipeline.Step[int, int]{step, step, step, step, step, step}, dummyReducer,
 			),
 		),
 	)
@@ -133,15 +83,21 @@ func Test_GraphRendering(t *testing.T) {
 // Also, the graph contains all conditionals returning the "worse" possible path (the largest way)
 //
 // The current graph has 26 steps, the UML can be seen at pipeline_benchmark_test.svg
-// BenchmarkPipeline_Run-8   	   62509	     21930 ns/op (0.02ms)	    2723 B/op	      82 allocs/op
+// goos: darwin
+// goarch: amd64
+// cpu: Intel(R) Core(TM) i7-1068NG7 CPU @ 2.30GHz
+// BenchmarkPipeline_Run-8   	   45620	     23057 ns/op	    5476 B/op	      80 allocs/op
 // Given this graph magnitude, the cost of traversing it is negligible in comparison to a step operation.
 func BenchmarkPipeline_Run(b *testing.B) {
+	var err error
 	graph := NewImmenseGraph()
+	ctx := context.Background()
+	in := 0
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		b.StartTimer()
-		_, err := graph.Run(context.Background(), "0")
+		_, err = graph.Run(ctx, in)
 		b.StopTimer()
 
 		if err != nil {
